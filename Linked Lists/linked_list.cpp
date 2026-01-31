51cad57a14fe50ffa756925c05eccf28cf1e6f5f
Ilyan321 <ilyaankhan342@gmail.com>
2026-01-31 14:30:59 +0500

    Linked lists Practice

--- Linked Lists/linked_list.cpp
+++ Linked Lists/linked_list.cpp
@@ -7,30 +7,48 @@ class Node{
 	Node* next;
 	Node(int val){
 		data=val;
-		next=NULL;}};
-		
+		next=NULL;
+	}
+};
+
+
+
+
+// class List{
+// 	public:
+// 	Node* head;
+// 	Node* tail;
+// 	List(){head=tail=NULL;}
+
+//     void push_front(int val){
+//         Node* newnode= new Node(val);
+//         if(head==NULL){
+//             head=tail=newnode;
+//             return;
+//         }
+//     }
+// };
 class List{
 	public:
 	Node* head;
-	Node* tail;
-	List(){head=tail=NULL;}
-
-    void push_front(int val){
-        Node* newnode= new Node(val);
-        if(head==NULL){
-            head=tail=newnode;
-            return;
-        }
-    }
-
-
-
-
-
+	Node*tail;
+	List(){
+		head=tail=NULL;
+	}
+	void push_front(int val){
+		Node* newnode=new Node(val);
+		if(head==NULL){
+			head=tail=newnode;
+			return;
+		}
+	}
 };
 	
 int main(){
-	
+	List ll;
+	ll.push_front(10);
+	ll.push_front(20);
+	ll.push_front(30);
 	cout<<"Linked List created successfully"<<endl;
 	
 return 0;	
---
+++ Linked Lists/tempCodeRunnerFile.cpp
@@ -0,0 +1,14 @@
+class List{
+// 	public:
+// 	Node* head;
+// 	Node* tail;
+// 	List(){head=tail=NULL;}
+
+//     void push_front(int val){
+//         Node* newnode= new Node(val);
+//         if(head==NULL){
+//             head=tail=newnode;
+//             return;
+//         }
+//     }
+// };
