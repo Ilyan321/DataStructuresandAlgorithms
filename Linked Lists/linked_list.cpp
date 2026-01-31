84ce4162e2ed49996dd4228a25484aa9b14d2f69
Ilyan321 <ilyaankhan342@gmail.com>
2026-01-31 15:48:08 +0500

    Linked lists Practice

--- Linked Lists/linked_list.cpp
+++ Linked Lists/linked_list.cpp
@@ -10,24 +10,7 @@ class Node{
 		next=NULL;
 	}
 };
-
-
-
-
-// class List{
-// 	public:
-// 	Node* head;
-// 	Node* tail;
-// 	List(){head=tail=NULL;}
-
-//     void push_front(int val){
-//         Node* newnode= new Node(val);
-//         if(head==NULL){
-//             head=tail=newnode;
-//             return;
-//         }
-//     }
-// };
+//Linked List class
 class List{
 	public:
 	Node* head;
@@ -35,21 +18,47 @@ class List{
 	List(){
 		head=tail=NULL;
 	}
+	//push front function
 	void push_front(int val){
 		Node* newnode=new Node(val);
-		if(head==NULL){
+		if (head==NULL){
 			head=tail=newnode;
 			return;
+		}else{
+			newnode->next=head;
+			head=newnode;
 		}
 	}
-};
-	
+	//push back function
+	void push_back(int val){
+		Node* newnode = new Node(val);
+		if (head==NULL){
+			head=tail=newnode;
+		}else{
+			tail->next=newnode;
+			tail=newnode;
+		}
+	}
+	//print linked list
+	void printll(){
+		Node* temp=head;
+		cout<<"Linked list: ";
+		while(temp!=NULL){
+			cout<<temp->data<<" ";
+			temp=temp->next;
+		}
+	}
+
+
+};//Linked List class end
 int main(){
 	List ll;
-	ll.push_front(10);
+	ll.push_back(30);
+	ll.push_back(40);
+	ll.push_back(50);
 	ll.push_front(20);
-	ll.push_front(30);
-	cout<<"Linked List created successfully"<<endl;
+	ll.push_front(10);
+	ll.printll();
 	
 return 0;	
 }
