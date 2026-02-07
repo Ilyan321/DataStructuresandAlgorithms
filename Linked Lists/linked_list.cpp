93b758743ded770cd8d6f2b207281da8d6b0e9bd
Ilyan321 <ilyaankhan342@gmail.com>
2026-02-07 13:24:23 +0500

    singly linked list

--- Linked Lists/linked_list.cpp
+++ Linked Lists/linked_list.cpp
@@ -18,10 +18,10 @@ class List{
 		head=tail=NULL;
 	}
 
-	//push_front
+	//push front
 	void push_front(int val){
 		Node* newnode=new Node(val);
-		if (head==NULL){
+		if(head==NULL){
 			head=tail=newnode;
 			return;
 		}else{
@@ -31,9 +31,9 @@ class List{
 	}
 
 	//push back
-	void push_back(int val){
+	void push_back (int val){
 		Node* newnode=new Node(val);
-		if(head==NULL){
+		if (head==NULL){
 			head=tail=newnode;
 			return;
 		}else{
@@ -41,7 +41,62 @@ class List{
 			tail=newnode;
 		}
 	}
-	//print linked list
+
+	//pop front
+	void pop_front(){
+		if(head==NULL){
+			cout<<"Linked list empty.\n";
+			return;
+		}head=head->next;	
+	}
+
+	//pop back
+	void pop_back(){
+		if(head==NULL){cout<<"Empty list\n";}
+		if(head==tail){
+			delete head;
+			head=tail=NULL;
+			return;}
+		Node* temp=head;
+		while(temp->next!=tail){
+			temp=temp->next;
+		}
+		delete tail;
+		tail=temp;
+		tail->next=NULL;
+	}
+
+	//counter
+	void counter(){int counter=0;
+		Node* temp=head;
+		while(temp!=NULL){
+			temp=temp->next;
+			counter++;
+		}cout<<"Total Nodes: "<<counter<<endl;
+	}
+
+	//search node
+	void search(){int node;int counter=1;bool found=false;
+		cout<<"Enter node data to find: ";cin>>node;
+		Node* temp=head;
+		while(temp!=NULL){
+			if(temp->data==node){
+				cout<<"Node "<<node<<" found at Location "<<counter<<endl;
+				found=true;
+				break;
+			}counter++;
+			temp=temp->next;
+		}
+		if(!found){
+			cout<<"Node not found\n";
+		}
+	}
+
+
+
+
+
+
 	void printll(){
 		Node* temp=head;
 		cout<<"Linked list: ";
@@ -51,45 +106,15 @@ class List{
 		}
 	}
 
-	//pop front
-	void pop_front(){
-		if(head==NULL){
-			cout<<"Linked list empty."<<endl;
-			return;
-		}
-		Node* temp =head;
-		head=head->next;
-		if (head==NULL){
-			tail=NULL;
-		}delete temp;
-	}
-	//pop back
-	void pop_back(){
-		if (head==NULL){
-			cout<<"Empty list.\n";
-			return;
-		}
-		if (head==tail){
-			delete head;
-			head=tail=NULL;
-			return;
-		}
-		Node* temp= head;
-		while (temp->next!=tail){
-			temp=temp->next;
-		}
-		delete tail;
-		tail=temp;
-		tail->next=NULL;
-	}
-
-
 };
-
 int main(){
 	List ll;
-	ll.push_back(10);
-	ll.push_front(100);
-	ll.pop_back();
+	ll.push_front(10);
+	ll.push_front(5);
+	ll.push_back(15);
+	ll.counter();
+	ll.search();
+	// ll.pop_front();
+	// ll.pop_back();
 	ll.printll();
 }
