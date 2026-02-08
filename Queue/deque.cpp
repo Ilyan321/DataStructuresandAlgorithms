bff542a52f9a9aa177aa9b7d20eaf0d8d911a7c8
Ilyan321 <ilyaankhan342@gmail.com>
2026-02-08 17:41:37 +0500

    deque using ll

---
+++ Queue/deque.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+class Node{
+    int data;
+    Node* next;
+    Node* prev;
+    Node(int val){data=val;next=prev=NULL;}
+};
+
+class deque{
+    public:
+    Node* front;
+    Node* rear;
+    deque(){front=rear=NULL;}
+
+    //push
+    
+
+
+
+};
